7cf3b8a7fa13e8704d5f2cd0c8932ae281fbecbb
Anna Belikova <bel4onok_spb@mail.ru>
2020-05-16 18:58:06 +0300

    About and contact components redesign

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -11,51 +11,63 @@ function About(props) {
 
     return(
         <div className='col-12 col-md-9 main_block'>
-            <div className="row">
-                <Breadcrumb>
+            <div className="row breadcrumb__row">
+                <Breadcrumb className="custom-breadcrumb">
                     <BreadcrumbItem><Link to="/home">Главная</Link></BreadcrumbItem>
                     <BreadcrumbItem active>О проекте</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>О проекте</h3>
-                    <hr />
-                </div>                
+                </Breadcrumb>              
             </div>
-            <div className="row row-content">
-                <div className="col-12 col-md-12 col-lg-6 ">
-                    <p>Проект только развивается. Наша цель - помочь роллерам комфортно передвигаться по городу и вовремя узнавать о перекопанных улицах и плохих дорогах, которые могут не только уменьшить удовольствие от поездки, но и навредить здоровью.</p>
+            <hr />
+            <div className="col-12 about__content">
+                <blockquote className="blockquote">
+                    <p >Там, где асфальт, ничего интересного, а&nbsp;где интересно,<br/> там нет асфальта.</p>
+                    <footer className="blockquote-footer">Аркадий и Борис Стругацкие,
+                    <cite title="Source Title">citatnica.ru</cite>
+                    </footer>
+                </blockquote>
+                <hr/>
+                <div className=" row-content about__row">
+                    <div className="col-12 col-md-12 col-lg-6">
+                        <p className="about__info">Проект только развивается. Наша цель - помочь роллерам комфортно передвигаться по городу и вовремя узнавать о перекопанных улицах и плохих дорогах, которые могут не только уменьшить удовольствие от поездки, но и навредить здоровью.</p>
+
+                    </div>
+                    <div className="col-12 col-md-12 col-lg-6">
+                        <Card className="about__card">
+                            <CardHeader className="about__card-header">Информация о проекте</CardHeader>
+                            <CardBody className="about__card-body">
+                                <dl className="row p-1">
+                                    <dt className="col-6">Начало работы над проектом</dt>
+                                    <dd className="col-6">08 апреля 2020</dd>
+                                    <dt className="col-6">Инициаторы</dt>
+                                    <dd className="col-6">2</dd>
+                                </dl>
+                            </CardBody>
+                        </Card>
+                    </div>
+                </div>
+                <div className=" about__team">
+                    <div className="about__title">Наша команда</div>
+                    <div className="about__items">
+                        <div className="about__item col-4">
+                            <div className="about__img"></div>
+                            <div className="about__surname">Винеров</div>
+                            <div className="about__name">Илья</div>
+                            <div className="about__biography">Главный идеолог, разработчик карты&nbsp;и, конечно&nbsp;же, роллер. Большая часть маршрутов на&nbsp;сайте&nbsp;&mdash; его детище.</div>
+
+                        </div>
+                        <div className="about__item col-4">
+                            <div className="about__img"></div>
+                            <div className="about__surname">Винеров</div>
+                            <div className="about__name">Илья</div>
+                            <div className="about__biography">Главный идеолог, разработчик карты&nbsp;и, конечно&nbsp;же, роллер. Большая часть маршрутов на&nbsp;сайте&nbsp;&mdash; его детище.</div>
+
+                        </div>
+                    </div>
                     
-                </div>
-                <div className="col-12 col-md-12 col-lg-5">
-                    <Card>
-                        <CardHeader className="bg-primary text-white">Факты</CardHeader>
-                        <CardBody>
-                            <dl className="row p-1">
-                                <dt className="col-6">Начало работы над проектом</dt>
-                                <dd className="col-6">08 апреля 2020</dd>
-                                <dt className="col-6">Начальный капитал</dt>
-                                <dd className="col-6">0 рублей и банка кофе</dd>
-                                <dt className="col-6">Инициаторы</dt>
-                                <dd className="col-6">2</dd>
-                            </dl>
-                        </CardBody>
-                    </Card>
-                </div>
-                <div className="col-12">
-                    <Card>
-                        <CardBody className="bg-faded">
-                            <blockquote className="blockquote">
-                                <p className="mb-0">А какой смысл покупать машину, чтобы разъезжать по асфальту? Там, где асфальт, ничего интересного, а где интересно, там нет асфальта.</p>
-                                <footer className="blockquote-footer">Аркадий и Борис Стругацкие,
-                                <cite title="Source Title">citatnica.ru</cite>
-                                </footer>
-                            </blockquote>
-                        </CardBody>
-                    </Card>
-                </div>
+                </div>              
             </div>
-         
-        </div>
+            </div>
+   
     );
 }
 
--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -29,125 +29,117 @@ class Contact extends Component {
         
            return(
         <div className="col-12 col-sm-11 col-md-9 main_block">
-        <div className="row">
-                <Breadcrumb>
+            <div className="row breadcrumb__row">
+                <Breadcrumb className="custom-breadcrumb">
                     <BreadcrumbItem><Link to="/home">Главная</Link></BreadcrumbItem>
                     <BreadcrumbItem active>Контакты</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>Контакты</h3>
-                    <hr />
-                </div>                
+                </Breadcrumb>               
             </div>
-            <div className="row row-content">
-                <div className="col-12 col-sm-4 offset-sm-1">
-                         
-                    <address>
-		              Murino<br />
-		              Shuvalova 9, 9<br />
-		              <i className="fa fa-phone fa-lg"></i>: +852 1234 5678<br />
-		              <i className="fa fa-fax fa-lg"></i>: +852 8765 4321<br />
-		              <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:skate_map@skating.net">
-                         skate_map@skating.net</a>
-                    </address>
-                </div>
-                <div className="col-12 col-sm-6 offset-sm-1">
-                    <div className="btn-group" role="group">
-                        <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
-                        <a href='/' role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
-                        <a role="button" className="btn btn-success" href="mailto:skate_map@skating.net"><i className="fa fa-envelope-o"></i> Email</a>
+            <hr/>
+                <div className="contacts__content">
+                <div className="row row-content contacts__row">
+                    <div className="col-12 col-sm-4">    
+                        <div className="contacts">
+                          <a className="contacts__phone" href="/"><i className="fa fa-phone fa-lg"></i> +852 1234 5678</a>
+                          <a className="contacts__email" href="mailto:bel4onok_spb@mail.ru"><i className="fa fa-envelope fa-lg"></i>
+                              bel4onok_spb@mail.ru</a>
+                        </div>
+                    </div>
+                    <div className="col-12 col-sm-3">
+                            <a role="button" className="contacts__btn" href="tel:+85212345678"><i className="fa fa-phone"></i></a>
+                            <a role="button" className="contacts__btn" href="mailto:skate_map@skating.net"><i className="fa fa-envelope-o"></i></a>
                     </div>
                 </div>
-             
-            </div>
-                <div className="row row-content">
-                   <div className="col-12">
-                      <h3>Обратная связь</h3>
-                   </div>
-                    <div className="col-12 col-md-9">
-                        <Form model="feedback" onSubmit={(values)=>this.handleSubmit(values)}>
-                            <Row className='form-group'>
-                                <Label htmlFor="firstname" md={2}>Имя</Label>
-                                <Col md={10}>
-                                    <Control.text model='.firstname' id="firstname"     name="firstname"
-                                        placeholder="Как к Вам обращаться?"
-                                        className='form-control' validators={{
-                                            required, minLength: minLength(3), maxLength: maxLength(15)
-                                        }} />
-                                        <Errors
+               <hr/>
+                    <div className="row row-content">
+                       <div className="col-12 contacts__form">
+                          <h3>Обратная связь</h3>
+                       </div>
+                        <div className="col-12 col-md-9">
+                            <Form model="feedback" onSubmit={(values)=>this.handleSubmit(values)}>
+                                <Row className='form-group'>
+                                    <Label htmlFor="firstname" >Как к Вам обращаться?</Label>
+                                    <Col md={12}>
+                                        <Control.text model='.firstname' id="firstname"     name="firstname"
+                                            className='form-control dark' validators={{
+                                                required, minLength: minLength(3), maxLength: maxLength(15)
+                                            }} />
+                                            <Errors
+                                                className="text-danger"
+                                                model=".firstname"
+                                                show="touched"
+                                                messages={{
+                                                required: 'Нужно заполнить. ',
+                                                minLength: 'Имя должно быть длиннее 2 символов. ',
+                                                maxLength: 'Имя должно быть короче 15 символов. '
+                                            }}
+                                         />
+
+                                    </Col>
+                                </Row>
+                                <Row className='form-group'>
+                                    <Label htmlFor="telnum" >Номер телефона</Label>
+                                    <Col md={12}>
+                                        <Control.text model='.telnum' id="telnum" name="telnum"
+                                            className='form-control dark'  validators={{
+                                                required, minLength: minLength(3), maxLength: maxLength(15), isNumber
+                                            }}/>
+                                            <Errors
                                             className="text-danger"
-                                            model=".firstname"
+                                            model=".telnum"
                                             show="touched"
                                             messages={{
-                                            required: 'Нужно заполнить. ',
-                                            minLength: 'Имя должно быть длиннее 2 символов. ',
-                                            maxLength: 'Имя должно быть короче 15 символов. '
-                                        }}
-                                     />
-                                    
-                                </Col>
-                            </Row>
-                            <Row className='form-group'>
-                                <Label htmlFor="telnum" md={2}>Телефон</Label>
-                                <Col md={10}>
-                                    <Control.text model='.telnum' id="telnum" name="telnum"
-                                        placeholder="Номер телефона"
-                                        className='form-control'  validators={{
-                                            required, minLength: minLength(3), maxLength: maxLength(15), isNumber
-                                        }}/>
+                                                required: 'Нужно заполнить. ',
+                                                minLength: 'Телефон должен быть длиннее 2 символов. ',
+                                                maxLength: 'Телефон должен быть короче 15 символов. ',
+                                                isNumber: 'Должен быть числом.'
+                                            }}
+                                         />
+
+                                    </Col>
+                                </Row>
+                                <Row className='form-group'>
+                                    <Label htmlFor="email">Email</Label>
+                                    <Col md={12}>
+                                        <Control.text model='.email' id="email" name="email"
+                                            className='form-control dark' validators={{
+                                                required, validEmail
+                                            }}/>
                                         <Errors
-                                        className="text-danger"
-                                        model=".telnum"
-                                        show="touched"
-                                        messages={{
-                                            required: 'Нужно заполнить. ',
-                                            minLength: 'Телефон должен быть длиннее 2 символов. ',
-                                            maxLength: 'Телефон должен быть короче 15 символов. ',
-                                            isNumber: 'Должен быть числом.'
-                                        }}
-                                     />
-                                    
-                                </Col>
-                            </Row>
-                            <Row className='form-group'>
-                                <Label htmlFor="email" md={2}>Email</Label>
-                                <Col md={10}>
-                                    <Control.text model='.email' id="email" name="email"
-                                        placeholder="Email"
-                                        className='form-control' validators={{
-                                            required, validEmail
-                                        }}/>
-                                    <Errors
-                                        className="text-danger"
-                                        model=".email"
-                                        show="touched"
-                                        messages={{
-                                            required: 'Нужно заполнить. ',
-                                            validEmail: 'Некорректный email.'
-                                        }}
-                                     />
-                                </Col>
-                            </Row>
-                         
-                            <Row className='form-group'>
-                                <Label htmlFor="message" md={2}>Сообщение</Label>
-                                <Col md={10}>
-                                    <Control.textarea model='.message' id="message" name="message"
-                                        rows="12"
-                                        className='form-control'
-                                        />
-                                </Col>
-                            </Row>
-                            <Row className='form-group'>
-                                <Col md={{size: 10, offset: 2}}>
-                                    <Button type="submit" color="primary">
-                                        Отправить
-                                    </Button>
-                                </Col>
-                            </Row>
-                        </Form>
-                    </div>
-               </div>
+                                            className="text-danger"
+                                            model=".email"
+                                            show="touched"
+                                            messages={{
+                                                required: 'Нужно заполнить. ',
+                                                validEmail: 'Некорректный email.'
+                                            }}
+                                         />
+                                    </Col>
+                                </Row>
+
+                                <Row className='form-group'>
+                                    <Label htmlFor="message">Сообщение</Label>
+                                    <Col md={12}>
+                                        <Control.textarea model='.message' id="message" name="message"
+                                            rows="12"
+                                            className='form-control dark'
+                                            />
+                                    </Col>
+                                </Row>
+                                <Row className='form-group'>
+                                    <Col>
+                                        <button className="submit-btn" type="submit" >
+                                            Отправить
+                                        </button>
+                                        <button className="reset-btn" type="reset" >
+                                            Сбросить
+                                        </button>
+                                    </Col>
+                                </Row>
+                            </Form>
+                        </div>
+                   </div>
+                </div>
         </div>
     );
     }
